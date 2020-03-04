04a45f79e869ca6313fa1c3a0edc851f477c1a3a
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-03-04 13:33:40 +0100

    Expose mediaEvents

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export * as Icons from './components/icons'
 export * as I18n from './common/labels'
 export inputMethodFactory from './components/input_method'
 export UploadTrigger from './components/UploadTrigger'
+export { mediaEvents } from './components/elements/Media'
 export { processText } from './components/elements/util'
 export NotificationManager from './components/NotificationManager'
 
