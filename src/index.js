5885fa7b1ac1dd15f182b06b758d2b24e08617ed
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-03-04 11:42:47 +0100

    Add shared modules to exports; remove unused files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import Chat from './components'
+
+export * as Icons from './components/icons'
+export { processText } from './components/elements/util'
+export NotificationManager from './components/NotificationManager'
+
 export default Chat
