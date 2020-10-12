367380d2c8a21814923349c0b1cdf19aad44d8a8
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-10-12 16:05:36 +0200

    dev stuff

--- dev/example.js
+++ dev/example.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import WebClient from '../src/index';
+import WebClient from '../src/index'
 import './example.scss'
 
-const user_id = 'u' + Math.random()
+const user_id = 'u' //+ Math.random()
 const bot_id = '1adc3f20-32e9-4376-a147-d9ef23ac8a4c'
 
 function App() {
@@ -14,14 +14,12 @@ function App() {
         <WebClient
           bot_id={bot_id}
           params={{ user_id }}
-          hideAvatars
           settings={{ alwaysFocus: true, chat_config: { disabled_inputs: [] } }}
         />
       </div>
     </div>
-  );
+  )
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('root'))
