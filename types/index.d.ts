cd4ec2abbfa7d89750751eb3f00a425311a6f05f
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-11-11 16:30:15 +0100

    NotificationManager types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,25 @@
+import React from 'react'
+import { Channel } from 'phoenix'
+
 declare module '@botsquad/web-client' {
   export function processText(input: string): { __html: any }
 
   export const I18n = {
-    resolveTranslations: (any, any) => any,
+    resolveTranslations: (input: any, locales: string[]) => any,
   }
 
   export type I18nValue = { $i18n: true }
   export type I18nArray = { [lang: string]: string[] } | I18nValue
+
+  export class NotificationManager {
+    constructor(component: React.Component)
+    componentDidMount(): void
+    setChannel(channel: Channel): void
+    sendChatOpenState(): void
+    onFocus(): void
+    notifyMessageCount(count: number): void
+    isHidden(): boolean
+    play(): void
+    closeConversation(g: string): void
+  }
 }
