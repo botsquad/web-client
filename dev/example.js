37c7474665da8d0d821756b8b824fc4bdf1c600d
Yiannis Christos Psaradakis <psaradakisJ@gmail.com>
2021-11-15 16:43:04 +0100

    [feat] Add dev environment variables / notes (#36)
    * [feat] added dev environment variables
    
    * [feat] added development notes
    
    * [fix] environment variable removed from components and added to example
    
    * [fix] Readme

--- dev/example.js
+++ dev/example.js
@@ -7,7 +7,7 @@ import './example.scss'
 
 const user_id = 'u2'
 const bot_id = '5b9e0beb-3c3d-4567-a0f4-63fa02fd588b'
-
+const mapsApiKey = process.env.GOOGLE_MAPS_API_KEY
 //const socket = new Socket('ws://localhost:4000/socket')
 //socket.connect()
 
@@ -18,6 +18,7 @@ function App() {
         <WebClient
           bot_id={bot_id}
           params={{ user_id }}
+          mapsApiKey={mapsApiKey}
           hideAvatars
           settings={{ alwaysFocus: true, chat_config: { disabled_inputs: [] } }}
         />
