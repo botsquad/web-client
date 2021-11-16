c2127c3dd195516a038c9ebc05faba59f727c684
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-11-16 18:07:25 +0100

    [feat] Text-> functional & typescript

---
+++ src/components/elements/Text.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import classNames from 'classnames'
+import { TextUtil } from '@botsquad/sdk'
+import { TextMessage } from './types'
+
+interface Props {
+  className: string
+  message: TextMessage
+}
+
+const Text: React.FC<Props> = ({ className, message }) => {
+  if (!message.payload.message || !message.payload.message.trim().length) {
+    return null
+  }
+
+  const emoji = /^\p{Emoji}$/u.test(message.payload.message.trim())
+
+  return (
+    <div className={classNames(className, { 'large-emoji': emoji })}>
+      <span dangerouslySetInnerHTML={TextUtil.processText(message.payload.message)} />
+    </div>
+  )
+}
+
+export default Text
