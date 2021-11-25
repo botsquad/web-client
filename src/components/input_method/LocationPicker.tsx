96d5b233fd1b69d4050593633096b19b6cefaa5c
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-11-25 16:24:17 +0100

    [refactor] Location Picker to typescript

---
+++ src/components/input_method/LocationPicker.tsx
@@ -0,0 +1,152 @@
+import React, { ReactNode, useEffect, useState } from 'react'
+import { compose, withProps } from 'recompose'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+
+import { chatLabel } from '../../common/labels'
+import { MyLocation } from '../icons'
+import InputMethodContainer from './InputMethodContainer'
+import { Location as LocationType } from '../elements/types'
+
+function transformLngLon(position: LocationType) {
+  return position ? { lat: position.lat, lng: position.lon } : null
+}
+
+interface MapProps {
+  center: LocationType
+  position: LocationType
+  config: { zoom?: number }
+  onChange: ([string]: any) => void
+  handler: any
+}
+
+const Map: React.FC<MapProps> = props => {
+  const [map, setMap] = useState<GoogleMap | null>()
+
+  const setMarkerPosition = ({ latLng }) => {
+    const center = { lat: map!.getCenter().lat(), lon: map!.getCenter().lng() }
+    const position = { lat: latLng.lat(), lon: latLng.lng() }
+    props.onChange({ position, center })
+  }
+
+  const { config, position } = props
+
+  const googleMapsPosition = transformLngLon(position)
+  const center = transformLngLon(props.center)
+
+  return (
+    <GoogleMap
+      ref={m => {
+        setMap(m)
+      }}
+      defaultZoom={config.zoom}
+      defaultCenter={center ? center : undefined}
+      center={center ? center : undefined}
+      onClick={setMarkerPosition}
+      options={{
+        disableDefaultUI: true,
+        zoomControl: true,
+        fullscreenControl: true,
+        clickableIcons: false,
+        mapTypeId: 'roadmap',
+      }}
+    >
+      {googleMapsPosition ? <Marker position={googleMapsPosition} draggable onDragEnd={setMarkerPosition} /> : null}
+    </GoogleMap>
+  )
+}
+
+const ComposedMap = compose<MapProps, { children?: ReactNode }>(
+  withProps((props: MapProps) => {
+    const mapsApiKey = props.handler.getMapsAPIKey()
+
+    return {
+      googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${mapsApiKey}&libraries=geometry,drawing,places`,
+      loadingElement: <div style={{ height: '100%', width: '100%' }} />,
+      containerElement: <div style={{ height: '100%', width: '100%' }} />,
+      mapElement: <div style={{ height: '100%', width: '100%' }} />,
+    }
+  }),
+  withScriptjs,
+  withGoogleMap,
+)(Map)
+
+interface LocationPickerProps {
+  config: any
+  inputModal: any
+  handler: any
+}
+const LocationPicker: React.FC<LocationPickerProps> = props => {
+  const [hasSubmitted, setHasSubmitted] = useState(false)
+  const [position, setPosition] = useState<LocationType | null>(null)
+  const [center, setCenter] = useState<LocationType | null>(null)
+  const [findingLocation, setFindingLocation] = useState(false)
+
+  useEffect(() => {
+    const { default_value, center: center2 } = props.config
+    if (default_value) {
+      setPosition(default_value)
+      setCenter(default_value)
+    } else {
+      setCenter(center2)
+      setTimeout(() => setMyLocation(), 100)
+    }
+  }, [])
+
+  const submit = () => {
+    setHasSubmitted(true)
+    props.inputModal.finish('location', position, props.config)
+  }
+
+  const setMyLocation = () => {
+    if (findingLocation) {
+      return
+    }
+    setFindingLocation(true)
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const position2 = { lat: coords.latitude, lon: coords.longitude }
+        setPosition(position2)
+        setCenter(position2)
+        setFindingLocation(false)
+      },
+      () => {
+        setFindingLocation(false)
+        alert('Could not retrieve your current position, please check your location settings.')
+      },
+    )
+  }
+
+  const setPositionAndCenter = ({ position, center }) => {
+    setPosition(position)
+    setCenter(center)
+  }
+
+  const { button_label } = props.config
+
+  return (
+    <InputMethodContainer
+      {...props}
+      className={`fixed-height location-picker ${findingLocation ? 'finding' : ''}`}
+      below={
+        !hasSubmitted ? (
+          <button disabled={position === null} onClick={submit}>
+            {button_label || chatLabel({ props }, 'location_picker_select')}
+          </button>
+        ) : null
+      }
+    >
+      <div className="btn my-location" onClick={setMyLocation}>
+        {MyLocation}
+      </div>
+      <ComposedMap
+        handler={props.handler}
+        onChange={setPositionAndCenter}
+        position={position}
+        center={center}
+        config={props.config}
+      />
+    </InputMethodContainer>
+  )
+}
+
+export default LocationPicker
