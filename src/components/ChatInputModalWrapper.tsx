b81dc5d069a02ce3e213dd09f0532fc5ecbf789b
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-11-26 15:10:01 +0100

    [refactor] ChatInputModalWrapper

---
+++ src/components/ChatInputModalWrapper.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import ChatInputModal from './ChatInputModal'
+
+interface ChatInputModalWrapperProps {
+  component: any
+  cancelLabel: string
+}
+
+const ChatInputModalWrapper: React.FC<ChatInputModalWrapperProps> = props => {
+  const cancel = () => {
+    const { component, cancelLabel } = props
+    if (component.state.inputMethodOverride) {
+      component.setState({ inputMethodOverride: null })
+    } else {
+      component.props.handler.send('message', { type: 'cancel', text: cancelLabel })
+    }
+  }
+
+  const finish = () => {
+    const { component } = props
+    component.setState({ inputMethodOverride: null })
+  }
+
+  const { children, component } = props
+  return (
+    <ChatInputModal
+      {...component.props}
+      onCancel={cancel}
+      onFinish={finish}
+      inputMethodOverride={component.state.inputMethodOverride}
+    >
+      {children}
+    </ChatInputModal>
+  )
+}
+
+export default ChatInputModalWrapper
