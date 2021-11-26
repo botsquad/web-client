76717fc09eb9f389e9999a82f3745a1a0ce8f21e
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-11-26 16:05:27 +0100

    [refactor] api

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,10 +1,14 @@
+import Message, { Payload } from './elements/types'
+
 export default class API {
+  _handler: any
+  _eventDispatcher: any
   constructor(handler, eventDispatcher) {
     this._handler = handler
     this._eventDispatcher = eventDispatcher
   }
 
-  sendMessage(message) {
+  sendMessage(message: Message<Payload>) {
     return this._handler.send('message', message)
   }
 
