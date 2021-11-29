8f52f4d10dd75598b27934ea0b9b63f61bd3c909
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-11-29 16:59:23 +0100

    [feat] added more values to context

--- src/components/input_method/InputMethodContext.tsx
+++ src/components/input_method/InputMethodContext.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import { Channel } from 'phoenix'
 import { createContext, ReactNode, useContext, useState } from 'react'
+import InputMethodTemplate from '../elements/InputMethodTemplate'
 import Message, { Payload } from '../elements/types'
 import { ChatHandler } from '../index'
 
@@ -19,15 +20,14 @@ interface InputMethodProps {
   conversationMeta: any
   events: Message<Payload>[]
   handler: ChatHandler
-}
-
-interface InputMethodUpdate {
-  setBotId: React.Dispatch<React.SetStateAction<string>>
-  setChannel: React.Dispatch<React.SetStateAction<Channel>>
-  setClassName: React.Dispatch<React.SetStateAction<string>>
-  setConversationMeta: React.Dispatch<React.SetStateAction<any>>
-  setEvents: React.Dispatch<React.SetStateAction<Message<Payload>[]>>
-  setHandler: React.Dispatch<React.SetStateAction<ChatHandler>>
+  hideAvatars: boolean
+  inline: boolean
+  inputModal: InputMethodTemplate
+  joined: boolean
+  layout: string
+  localePrefs: string[]
+  mapsApiKey: string
+  message: Message<Payload>
 }
 
 const DEFAULT_INPUT_METHOD_PROPS: InputMethodProps = {
@@ -37,6 +37,30 @@ const DEFAULT_INPUT_METHOD_PROPS: InputMethodProps = {
   conversationMeta: null,
   events: [],
   handler: null,
+  hideAvatars: false,
+  inline: false,
+  inputModal: null,
+  joined: false,
+  layout: undefined,
+  localePrefs: [],
+  mapsApiKey: '',
+  message: null,
+}
+
+interface InputMethodUpdate {
+  setBotId: React.Dispatch<React.SetStateAction<string>>
+  setChannel: React.Dispatch<React.SetStateAction<Channel>>
+  setClassName: React.Dispatch<React.SetStateAction<string>>
+  setConversationMeta: React.Dispatch<React.SetStateAction<any>>
+  setEvents: React.Dispatch<React.SetStateAction<Message<Payload>[]>>
+  setHandler: React.Dispatch<React.SetStateAction<ChatHandler>>
+  setHideAvatars: React.Dispatch<React.SetStateAction<boolean>>
+  setInline: React.Dispatch<React.SetStateAction<boolean>>
+  setInputModal: React.Dispatch<React.SetStateAction<InputMethodTemplate>>
+  setJoined: React.Dispatch<React.SetStateAction<boolean>>
+  setLocalePrefs: React.Dispatch<React.SetStateAction<string[]>>
+  setMapsApiKey: React.Dispatch<React.SetStateAction<string>>
+  setMessage: React.Dispatch<React.SetStateAction<Message<Payload>>>
 }
 
 const InputMethodPropsContext = createContext<InputMethodProps>(DEFAULT_INPUT_METHOD_PROPS)
@@ -48,10 +72,47 @@ const ElementFactoryProvider = ({ children }: { children: ReactNode }) => {
   const [className, setClassName] = useState(DEFAULT_INPUT_METHOD_PROPS.className)
   const [conversationMeta, setConversationMeta] = useState<any>(DEFAULT_INPUT_METHOD_PROPS.conversationMeta)
   const [events, setEvents] = useState<Message<Payload>[]>(DEFAULT_INPUT_METHOD_PROPS.events)
-  const [handler, setHandler] = useState<ChatHandler>()
-
-  const props = { botId, channel, className, conversationMeta, events, handler }
-  const updaters = { setBotId, setChannel, setClassName, setConversationMeta, setEvents, setHandler }
+  const [handler, setHandler] = useState<ChatHandler>(DEFAULT_INPUT_METHOD_PROPS.handler)
+  const [hideAvatars, setHideAvatars] = useState(DEFAULT_INPUT_METHOD_PROPS.hideAvatars)
+  const [inline, setInline] = useState(DEFAULT_INPUT_METHOD_PROPS.inline)
+  const [inputModal, setInputModal] = useState<InputMethodTemplate>(DEFAULT_INPUT_METHOD_PROPS.inputModal)
+  const [joined, setJoined] = useState(DEFAULT_INPUT_METHOD_PROPS.joined)
+  const [layout, setLayout] = useState<string>(DEFAULT_INPUT_METHOD_PROPS.layout)
+  const [localePrefs, setLocalePrefs] = useState(DEFAULT_INPUT_METHOD_PROPS.localePrefs)
+  const [mapsApiKey, setMapsApiKey] = useState(DEFAULT_INPUT_METHOD_PROPS.mapsApiKey)
+  const [message, setMessage] = useState<Message<Payload>>(DEFAULT_INPUT_METHOD_PROPS.message)
+  const props = {
+    botId,
+    channel,
+    className,
+    conversationMeta,
+    events,
+    handler,
+    hideAvatars,
+    inline,
+    inputModal,
+    joined,
+    layout,
+    localePrefs,
+    mapsApiKey,
+    message,
+  }
+  const updaters = {
+    setBotId,
+    setChannel,
+    setClassName,
+    setConversationMeta,
+    setEvents,
+    setHandler,
+    setHideAvatars,
+    setInline,
+    setInputModal,
+    setJoined,
+    setLayout,
+    setLocalePrefs,
+    setMapsApiKey,
+    setMessage,
+  }
 
   return (
     <InputMethodPropsContext.Provider value={props}>
