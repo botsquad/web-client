da03c086fd7d94b4cba3da72ccb6d5ccd136922f
Yiannis Christos Psaradakis <psaradakisJ@gmail.com>
2022-01-25 15:20:19 +0100

    [feat] Support IBAN in form widget
    Squashed commit of the following:
    
    commit 4f108a22849e3a38be3395aa38b58f8eb6c146d9
    Author: Arjan Scherpenisse <arjan@scherpenisse.net>
    Date:   Tue Jan 25 15:19:03 2022 +0100
    
        Remove IBAN placeholder
    
    commit 07d16479b0bd67123b531747a5065d1da793c0da
    Author: darthinvader <psaradakisj@gmail.com>
    Date:   Tue Jan 25 11:18:08 2022 +0200
    
        [fix] localization
    
    commit fda2656b6813151653abd2780fe780c21654cf47
    Author: darthinvader <psaradakisj@gmail.com>
    Date:   Mon Jan 24 15:44:31 2022 +0200
    
        [fix] form errors
    
    commit 42f1a462692013ca7cd98ea3256718cd989737cc
    Author: darthinvader <psaradakisj@gmail.com>
    Date:   Mon Jan 24 14:11:45 2022 +0200
    
        [feat] Support IBAN in form widget

--- src/components/input_method/FormWidgets.tsx
+++ src/components/input_method/FormWidgets.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Widget } from 'react-jsonschema-form'
+import { chatLabel } from '../../common/labels'
+import { electronicFormat, isValid, printFormat } from 'iban'
+import React, { useEffect, useState } from 'react'
+import { Widget, WidgetProps } from 'react-jsonschema-form'
 import PhoneInput, { Country } from 'react-phone-number-input'
 import 'react-phone-number-input/style.css'
 
@@ -64,4 +66,48 @@ class PhoneNumberWidget extends React.Component<PhoneNumberWidgetProps> {
   }
 }
 
-export default { phone_number: PhoneNumberWidget as unknown as Widget }
+const IbanFormWidget: React.FC<WidgetProps> = ({ value, onChange, disabled, autofocus, formContext, id }) => {
+  const [error, setError] = useState(true)
+
+  const onIBANChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = electronicFormat(e.target.value)
+    onChange(value)
+  }
+
+  useEffect(() => {
+    setTimeout(() => formContext.setWidgetError(id, error), 0)
+  }, [error])
+
+  useEffect(() => {
+    if (!isValid(value)) {
+      setError(true)
+    } else {
+      setError(false)
+    }
+  }, [value])
+
+  const validate = () => {
+    if (error) {
+      return <div style={{ color: 'red' }}>{chatLabel({ ui_labels: [] }, formContext.localePrefs, 'invalid_iban')}</div>
+    }
+    return null
+  }
+
+  return (
+    <div key={id}>
+      <input
+        type="text"
+        className="form-control"
+        onChange={onIBANChange}
+        value={printFormat(value || '')}
+        disabled={disabled}
+        autoFocus={autofocus}
+      ></input>
+      {validate()}
+    </div>
+  )
+}
+export default {
+  phone_number: PhoneNumberWidget as unknown as Widget,
+  iban_number: IbanFormWidget as unknown as Widget,
+}
