3b2a5c39adf0599babbe151983cc8029aeca08a9
Randall Theuns <randall.theuns@enreach.com>
2025-08-18 13:21:53 +0200

    Export AugmentedChannel

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ export * as Icons from './components/icons'
 export * as I18n from './common/labels'
 export type I18nValue = { $i18n: true }
 export type I18nArray = { [lang: string]: string[] } | I18nValue
+export type { AugmentedChannel } from './components/ChatContext'
 
 export { default as inputMethodFactory } from './components/input_method'
 export { default as UploadTrigger } from './components/UploadTrigger'
